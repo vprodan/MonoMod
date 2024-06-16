c775b21941662d38b7794f75662c1b966949bbb9
DaNike <danike@cirr.com>
2024-06-15 22:40:41 -0500

    Fix run-mono-tests

--- .github/workflows/run-mono-tests.ps1
+++ .github/workflows/run-mono-tests.ps1
@@ -1,9 +1,6 @@
 $useMdh = $env:USE_MDH -eq "true";
-$exe = if ($useMdh) { $env:MDH } else { $env:MONO_DLL };
-$exeargs = if ($useMdh) { @($env:MONO_DLL) } else { @() };
-
-echo $exe;
-echo $exeargs;
+[string]$exe = if ($useMdh) { $env:MDH } else { $env:MONO_DLL };
+[string[]]$exeargs = if ($useMdh) { @($env:MONO_DLL) } else { @() };
 
 # TODO: this xUnit TFM is incorrect for some targets. We'd really like to use vstest on .NET Mono, as that will keep things somewhat simpler.
 &$exe @exeargs "$($env:NUGET_PACKAGES)/xunit.runner.console/$($env:XunitVersion)/tools/$($env:TFM)/xunit.console.exe" `
